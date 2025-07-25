5a1e7ed78cf90aec65c649c765b037f27b51ccf4
SaurabhDevelopor <saurabhkumar13618@gmail.com>
2025-07-25 17:07:12 +0530

    upload and update files

--- server/routes/auth.js
+++
@@ -1,7 +0,0 @@
-const express=require("express");
-const { login } = require("../Controllers/Auth");
-const route=express.Router();
-
-route.post("/login",login);
-
-module.exports=route
--- youtube/src/app/feed/lv/page.jsx
+++
@@ -1,8 +0,0 @@
-import Liked_Video from '@/app/Components/LikedVideo/Liked_Video'
-import React from 'react'
-
-export default function page() {
-  return (
-    <Liked_Video/>
-  )
-}
